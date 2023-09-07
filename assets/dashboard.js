553b830fce2544e67a1139458758ee030b9c1495
dleovl <95427722+dleovl@users.noreply.github.com>
2023-09-07 00:17:46 -0400

    Local Name Sizing Fix and Username Linking

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,8 +1,90 @@
 'use strict';
-/* custom code */
+/* custom code p1 */
+// search "custom code" to see changes
 document.onmousemove = function () {
     document.title = "Dashboard";
 };
+$(function() {
+    // temp fix for long names
+    !function(t){var e,i,s,n;n="quickfit",s={min:8,max:12,tolerance:.02,truncate:!1,width:null,sampleNumberOfLetters:10,sampleFontSize:12},i=function(){var e=null;function i(e){this.options=e,this.item=t('<span id="meassure"></span>'),this.item.css({position:"absolute",left:"-1000px",top:"-1000px","font-size":""+this.options.sampleFontSize+"px"}),t("body").append(this.item),this.meassures={}}return i.instance=function(t){return e||(e=new i(t)),e},i.prototype.getMeassure=function(t){var e;return(e=this.meassures[t])||(e=this.setMeassure(t)),e},i.prototype.setMeassure=function(t){var e,i,s,n,h;for(i=0,n="",s=" "===t?"&nbsp;":t,h=this.options.sampleNumberOfLetters-1;0<=h?i<=h:i>=h;0<=h?i++:i--)n+=s;return this.item.html(n),e=this.item.width()/this.options.sampleNumberOfLetters/this.options.sampleFontSize,this.meassures[t]=e,e},i}(),e=function(){function e(e,h){this.$element=e,this.options=t.extend({},s,h),this.$element=t(this.$element),this._defaults=s,this._name=n,this.quickfitHelper=i.instance(this.options)}return e.prototype.fit=function(){var t;return this.options.width||(t=this.$element.width(),this.options.width=t-this.options.tolerance*t),(this.text=this.$element.attr("data-quickfit"))?this.previouslyTruncated=!0:this.text=this.$element.text(),this.calculateFontSize(),this.options.truncate&&this.truncate(),{$element:this.$element,size:this.fontSize}},e.prototype.calculateFontSize=function(){var t,e,i;for(i=0,e=0;i<this.text.length;++i)t=this.text.charAt(i),e+=this.quickfitHelper.getMeassure(t);return this.targetFontSize=parseInt(this.options.width/e),this.fontSize=Math.max(this.options.min,Math.min(this.options.max,this.targetFontSize))},e.prototype.truncate=function(){var t,e,i,s,n;if(this.fontSize>this.targetFontSize){for(s="",n=3*this.quickfitHelper.getMeassure(".")*this.fontSize,t=0;n<this.options.width&&t<this.text.length;)i=this.text[t++],e&&(s+=e),n+=this.fontSize*this.quickfitHelper.getMeassure(i),e=i;return s.length+1===this.text.length?s=this.text:s+="...",this.textWasTruncated=!0,this.$element.attr("data-quickfit",this.text).html(s)}if(this.previouslyTruncated)return this.$element.html(this.text)},e}(),t.fn.quickfit=function(t){for(var i=[],s=this.each(function(){var s=new e(this,t).fit();return i.push(s),s.$element}),n=0;n<i.length;n++){var h=i[n];h.$element.css({fontSize:h.size+"px"})}return s}}(jQuery,window);
+    $("#myName").quickfit();
+});
+
+function convertUri(text) { // converts string only
+    // regex may be changed to do maybe www.google.com and not just https://www.google.com or https://google.com
+    // i think exp2 is trying to do that but it just doesn't
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    var ctext = text.replaceAll(exp, "<a target=\"_blank\" style=\"text-decoration:none\" href=\"$1\">$1</a>");
+    var exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    return ctext.replaceAll(exp2, "$1<a target=\"_blank\" style=\"text-decoration:none\" href=\"https://$2\">$2</a>");
+}
+
+function convertHandle(text) {
+    var mentionRegex = /@(\w+)/g;
+    function replaceMentions(match, username) {
+        return `<a onclick="openChat('${username}')" href="javascript:void(0)" style="text-decoration:none;font-weight:bold">@${username}</a>`;
+    }
+    return text.replace(mentionRegex, replaceMentions);
+}
+
+var currentUser; // Curent opened username. Not sure if there is already a variable for this.
+function openChat(user) {
+	if (user != my.username) {
+	    if (user != currentUser) {
+    		$.post("processes", {
+    			process: "newChat",
+    			data: user
+    		})
+    		.then(response => {
+    			if (response = verifyResultJSON(response)) {
+    				if (!response.ok) {
+    					throw new Error(response.statusText);
+    				}
+    				try {
+    					my.chats[user].init();
+    				} catch (e) {
+    					let receiver = response.receiver.username;
+    					let $element = $(document.createElement("li"));
+    					$element.append(`
+                        <div class="wrapper">
+                            <div class="profile-picture-wrapper">
+                                <img src="${response.receiver.picture}">
+                                <div class="status-circle"></div>
+                            </div>
+                            <span>
+                                <strong>${receiver}</strong>
+                                <br>
+                                <span class="lastMsg"></span>
+                            </span>
+                        </div>
+                        `);
+    					$("#profiles-list").prepend($element);
+    					let chatObj = new Chat(receiver, response.receiver.name, response.receiver.picture, $element);
+    					chatObj.$element.on("click", () => chatObj.init());
+    					my.chats[receiver] = chatObj;
+    					chatObj.init();
+    					if (my.socket.available()) my.socket.send("S", receiver);
+    				}
+    			}
+    		})
+    		.catch(error => {
+    			Swal.fire(
+    				"Not Found",
+    				"@" + user + " not found.",
+    				"error"
+    			);
+    		});
+	    }
+	} else {
+		Swal.fire(
+			"This is you!",
+			"You cannot message yourself.",
+			"info"
+		);
+	}
+}
+
+/* end cc */
 
 let my = {
   username: $("#myUsername").text().replace("@", ""),
@@ -349,19 +431,14 @@ class Chat {
         result.messages.forEach(message => {
           // if the message doesn't have a subsequent type variable it is NOT media
           if (!message.type) {
+              currentUser = this.username;
             // generate the html text message
             /*
                 CUSTOM CODE 1/3
                 CONVERT NORMAL MESSAGES
                 THIS IS FOR WHEN MESSAGES ARE LOADED
             */
-            function convert(text) {
-                var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-                var ctext = text.replace(exp, "<a target=\"_blank\" style=\"text-decoration:none\" href=\"$1\">$1</a>");
-                var exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-                return ctext.replace(exp2, "$1<a target=\"_blank\" style=\"text-decoration:none\" href=\"https://$2\">$2</a>");
-            }
-            var convertedMsg = convert(message.content.replaceAll("\n", "<br>"));
+            var convertedMsg = convertHandle(convertUri(message.content.replaceAll("\n", "<br>")));
             /* was:
             <span>${message.content.replaceAll("\n", "<br>")}</span>
             */
@@ -514,13 +591,7 @@ class Chat {
         STORE NORMAL TEXT IN DB, ONLY CHANGE WHAT YOU SEE
         THIS IS FOR WHEN MESSAGES ARE SENT BY YOU
     */
-    function convert(text) { // converts string only
-        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-        var ctext = text.replace(exp, "<a target=\"_blank\" style=\"text-decoration:none\" href=\"$1\">$1</a>");
-        var exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-        return ctext.replace(exp2, "$1<a target=\"_blank\" style=\"text-decoration:none\" href=\"https://$2\">$2</a>");
-    }
-    var visualMsg = convert(escapeHtml(message).replaceAll("\n", "<br>"));
+    var visualMsg = convertHandle(convertUri(escapeHtml(message).replaceAll("\n", "<br>")));
     /* was:
         $newMsg.find("span").html(escapeHtml(message).replaceAll("\n", "<br>"))
     */
@@ -603,13 +674,7 @@ class Chat {
             CONVERT NORMAL MESSAGES
             THIS IS FOR WHEN MESSAGES ARE RECIEVED
         */
-        function convert(text) { // converts string only
-            var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-            var ctext = text.replace(exp, "<a target=\"_blank\" style=\"text-decoration:none\" href=\"$1\">$1</a>");
-            var exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-            return ctext.replace(exp2, "$1<a target=\"_blank\" style=\"text-decoration:none\" href=\"https://$2\">$2</a>");
-        }
-        var visualMsg = convert(message.content.replaceAll("\n", "<br>"));
+        var visualMsg = convertHandle(convertUri(message.content.replaceAll("\n", "<br>")));
         document.title = "💬 Dashboard"
         /* was:
             <span>${message.content.replaceAll("\n", "<br>")}</span>
