effad3dcebcd02ec008526f2a79ffd387ef705af
Griffin Garman <coppergriffing@gmail.com>
2023-09-24 13:23:43 -0400

    Adds video seeking (closes #12)
    Uses a buffer to load videos quicker and enable video seeking as well as resumable downloads. Downside to this is increased cpu usage and occasional video buffering.

--- media.php
+++ media.php
@@ -86,19 +86,56 @@ if ($userId != $row["sender"] && $userId != $row["receiver"]) {
   die();
 }
 
+$file = "$privateFolder/chats/media/" . $row["filename"];
+
 // Check that media file exists
-if (!is_file("$privateFolder/chats/media/" . $row["filename"])) {
+if (!is_file($file)) {
   http_response_code(404);
   // Include 404 error document here
   die();
 }
 
-$mediaContent = file_get_contents(
-  "$privateFolder/chats/media/" . $row["filename"]
-);
-
+// Must revalidate (remove for production)
 header("Cache-Control: no-cache, must-revalidate");
+
+// Ranges for resumable downloads and video seeking https://www.sitepoint.com/community/t/loading-html5-video-with-php-chunks-or-not/350957/3
+$fileStream = fopen($file, 'rb');
+$size   = filesize($file); // File size
+$length = $size;           // Content length
+$start  = 0;               // Start byte
+$end    = $size - 1;       // End byte
 header("Content-Type: " . $row["type"]);
+header("Accept-Ranges: bytes");
+
+// Handles partial content requests
+if (isset($_SERVER['HTTP_RANGE'])) {
+  $c_start = $start;
+  $c_end   = $end;
+  list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+  if (strpos($range, ',') !== false) {
+    header('HTTP/1.1 416 Requested Range Not Satisfiable');
+    header("Content-Range: bytes $start-$end/$size");
+    exit;
+  }
+  if ($range == '-') {
+    $c_start = $size - substr($range, 1);
+  } else {
+    $range  = explode('-', $range);
+    $c_start = $range[0];
+    $c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+  }
+  $c_end = ($c_end > $end) ? $end : $c_end;
+  if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+    header('HTTP/1.1 416 Requested Range Not Satisfiable');
+    header("Content-Range: bytes $start-$end/$size");
+    exit;
+  }
+  $start  = $c_start;
+  $end    = $c_end;
+  $length = $end - $start + 1;
+  fseek($fileStream, $start);
+  header('HTTP/1.1 206 Partial Content');
+}
 
 if (isset($_GET["download"])) {
   header("Content-Description: File Transfer");
@@ -108,10 +145,31 @@ if (isset($_GET["download"])) {
     "Content-Disposition: attachment; filename=\"" . $row["original"] . "\""
   );
   header(
-    "Content-Length: " .
-      filesize("$privateFolder/chats/media/" . $row["filename"])
+    "Content-Length: $size"
   );
   header("Pragma: public");
 }
 
-die($mediaContent);
+header("Content-Range: bytes $start-$end/$size");
+header("Content-Length: " . $length);
+$buffer = 1024 * 8;
+$s = 0;
+while (!feof($fileStream) && ($p = ftell($fileStream)) <= $end) {
+  if ($p + $buffer > $end) {
+    $buffer = $end - $p + 1;
+  }
+  $s++;
+  
+  echo fread($fileStream, $buffer);
+  if ($s >= 250) {
+    ob_clean();
+    ob_flush();
+    flush();
+    break;
+    
+  } else {
+    flush();
+  }
+}
+fclose($fileStream);
+exit();
